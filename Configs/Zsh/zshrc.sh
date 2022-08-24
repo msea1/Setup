44a27a53506ba5eef3a043871cec567cf0c5cc65
Matthew Carruth <carruthm@gmail.com>
2022-08-24 13:09:09 -0700

    update zshrc, got git branch to work (single quote PS1)

--- Configs/Zsh/zshrc.sh
+++ Configs/Zsh/zshrc.sh
@@ -5,7 +5,7 @@ case $- in
 esac
 
 
-### ENVIRONMENT VARIABLES ###
+# EnvVars
 export EDITOR=vim
 export HISTFILESIZE=20000
 export HISTSIZE=10000
@@ -15,28 +15,45 @@ export HISTTIMEFORMAT="%a %d %b %T: "
 export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
 export PROMPT_DIRTRIM=3
 
+
+# Include Git Branch
 autoload -Uz vcs_info
 precmd () { vcs_info }
-zstyle ':vcs_info:*' formats '(%F{green}%b%f)'
+zstyle ':vcs_info:*' formats '[%F{green}%b%f]'
+setopt PROMPT_SUBST
+PROMPT='%F{blue}%3~%f ${vcs_info_msg_0_} $ '
 
-PS1="%F{blue}%3~%f $vcs_info_msg_0_ $ "
+
+# direnv
+eval "$(direnv hook zsh)"
 
 
-### OPTIONS ###
+# nvm
+export NVM_DIR=~/.nvm
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+
+
+# Virtualenv
+eval "$(pyenv init -)"
+
+
+# Allow # in terminal
 setopt interactive_comments
 
 
-### PATHS ###
+# Case Insensitive Matching
+zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|=*' 'l:|=* r:|=*'
+autoload -Uz compinit && compinit
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 
-### ALIASES ###
+# Load aliases file
 if [ -f ~/.sh_aliases ]; then
 . ~/.sh_aliases
 fi
 
 
-### FUNCTIONS ###
+# Load functions file
 if [ -f ~/.sh_fxs ]; then
 . ~/.sh_fxs
 fi
-
