3b597d8f949f485b20d6f7494d352fea47b85439
Matthew Carruth <carruthm@gmail.com>
2022-09-20 16:44:16 -0700

    add pytest with coverage alias

--- Configs/Zsh/zsh_aliases.sh
+++ Configs/Zsh/zsh_aliases.sh
@@ -13,11 +13,11 @@ alias code='cd ~/Code'
 alias tempd='cd ~/Temp'
 
 # NEW COMMANDS
+alias covtest='poetry run pytest -cov --cov-report term-missing:skip-covered --durations=0 --durations-min=0.25'
 alias gitpersonal='git config user.email carruthm@gmail.com'
 alias gitwork='git config user.email Matthew@crescendohealth.co'
 alias ld='ls -ABF --color=auto'
 alias ll='ls -AhlF --color=auto'
-alias files='xdg-open . &>/dev/null &'
 alias root="sudo su -"
 alias sorry='sudo $(fc -ln -1)'
 alias sudo_pw='cat ~/.sudo_pw | xsel -ib'
