c8ec0bc10bf45047faa6b9b2db8dad576b36277e
Matthew Carruth <mcarruth@blacksky.com>
2023-01-26 22:54:19 -0800

    aws_creds

--- Shells/Bash/bash_fx.sh
+++ Shells/Bash/bash_fx.sh
@@ -13,6 +13,23 @@ author_by_file() {
   unset IFS
 }
 
+aws_creds() {
+  touch ~/.aws/credentials
+  echo -e '[default]' > ~/.aws/credentials
+  ttl=$(vault token lookup | tail -1 | awk '{print $2}')
+  if [[ $ttl -le 0 ]]; then
+    vauth
+  fi
+  vault read aws/creds/sfi-imaging | while read -a line; do
+    if [[ ${line[0]} == 'access_key' ]]; then
+      echo -e 'aws_access_key_id =' ${line[1]} >> ~/.aws/credentials
+    fi
+    if [[ ${line[0]} == 'secret_key' ]]; then
+      echo -e 'aws_secret_access_key =' ${line[1]} >> ~/.aws/credentials
+    fi
+  done
+}
+
 del_br() {
   local d=$(git rev-parse --abbrev-ref HEAD)
   g co master
