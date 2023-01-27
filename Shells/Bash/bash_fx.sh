08ba96583558fa233c19814ff98a7122df78d4ae
Matthew Carruth <mcarruth@blacksky.com>
2023-01-26 22:56:43 -0800

    rm parse_vpn

--- Shells/Bash/bash_fx.sh
+++ Shells/Bash/bash_fx.sh
@@ -86,28 +86,6 @@ parse_git_branch() {
   git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
 }
 
-parse_vpn() {
-  # used to use nmcli con show --active but no longer works w/AnyConnect
-  d=$(ip --json addr | grep broadcast | grep "172\.2")
-  if [ "$d" ]; then
-    e=$(echo $d | grep "\.19\.255")
-    if [ "$e" ]; then  # also ifindex 20
-      echo "(BSky)"
-      return
-    fi
-    e=$(echo $d | grep "\.1\.255")
-    if [ "$e" ]; then  # also ifindex 13
-      echo "(MOC)"
-      return
-    fi
-  fi
-  d=$(ip --json addr | grep broadcast | grep "192\.168\.215\.255")
-  if [ "$d" ]; then  # also, ifindex 21
-    echo "(LeoS)"
-    return
-  fi
-}
-
 release_diff() {
   # show changes in dir from $1 to $2
   echo Commits
